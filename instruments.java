446a56f7ef4962470a4390b6a89a89d82c26b0e1
Intanarumm <intanarumm75@gmail.com>
2025-09-09 13:30:52 +0700

    Add files via upload

---
+++ instruments.java
@@ -0,0 +1,15 @@
+public class instruments {
+    private String name;
+    private double price;
+
+    public instruments (){
+        name = "Kolintang";
+        price = 20000;
+    }
+    public void print (){
+        System.out.println("nama: " + name);
+        System.out.println("Harga: " + price);
+
+    }
+    
+}
---
+++ instrumentsTest.java
@@ -0,0 +1,7 @@
+public class instrumentsTest {
+    public static void main(String[] args) {
+        instruments kolintang = new instruments();
+        kolintang.print();
+    }
+    
+}
