5d2a1c9d3828e7f78b8104b65d022c2ff80e7283
Intanarumm <intanarumm75@gmail.com>
2025-09-10 09:32:36 +0700

    Add files via upload

--- instruments.java
+++ instruments.java
@@ -6,6 +6,27 @@ public class instruments {
         name = "Kolintang";
         price = 20000;
     }
+    //Constructor Parameter
+    public instruments(String n, int prc) {
+        name = n;
+        price = prc;
+
+    //Getter
+    }
+    public String getName(){
+        return name;
+    }
+    public double getPrice(){
+        return price;
+    }
+    
+    //Setter
+    public void setName(String n){
+        name = n;
+    }
+    public void setPrice (double prc){
+        price = prc;
+    }
     public void print (){
         System.out.println("nama: " + name);
         System.out.println("Harga: " + price);
--- instrumentsTest.java
+++ instrumentsTest.java
@@ -1,7 +1,9 @@
 public class instrumentsTest {
     public static void main(String[] args) {
-        instruments kolintang = new instruments();
+        instruments kolintang = new instruments("Soldier", 20000);
         kolintang.print();
+        instruments sasando = new instruments("Guardian",50000 );
+        sasando.print();
     }
     
 }
