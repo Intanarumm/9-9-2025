723d813e79a8b5bd09d41079cf87e9c46de99293
Intanarumm <intanarumm75@gmail.com>
2025-09-10 09:36:36 +0700

    Add files via upload

--- instrumentsTest.java
+++ instrumentsTest.java
@@ -1,6 +1,6 @@
 public class instrumentsTest {
     public static void main(String[] args) {
-        instruments kolintang = new instruments("Soldier", 20000);
+        instruments kolintang = new instruments("Soldier", 2000)
         kolintang.print();
         instruments sasando = new instruments("Guardian",50000 );
         sasando.print();
